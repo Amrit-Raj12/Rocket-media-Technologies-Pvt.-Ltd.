46e61070f560a0861e8d9eb78cf1a4be423ccb2a
Amrit-Raj12 <67092088+Amrit-Raj12@users.noreply.github.com>
2020-10-20 12:45:18 +0530

    Update pairs_of_socks.js
    JavaScript(Node.JS)

--- pairs_of_socks.js
+++ pairs_of_socks.js
@@ -9,7 +9,9 @@ i=1;
 while(i<=(ar.length-1)){
   if(ar[i-1]==ar[i]){
     count += 1;
+    i += 2;
+  }else{
+    i += 1;
   }
-  i=i+2;
 }
 console.log(`total number of matching pairs of socks that Rohit can sell ${count}`);
