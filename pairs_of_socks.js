ab77fff90f72976c3c6f4d328d90d9eeaae191c0
Amrit-Raj12 <67092088+Amrit-Raj12@users.noreply.github.com>
2020-10-20 12:29:31 +0530

    Update pairs_of_socks.js

--- pairs_of_socks.js
+++ pairs_of_socks.js
@@ -1,17 +1,15 @@
 const prompt=require('prompt-sync')();
-const N=prompt("Enter the size of array");
+const N=prompt("the number of socks represented ");
 var ar=[];
 for(let i=0;i<N;i++){
-  ar[i]=prompt('Enter Element for Array1 ');
+  ar[i]=prompt('separated integer describing the colors ');
 }
 count=0;
-var i=1;
-while(i<((ar.length)-1)){
+i=1;
+while(i<=(ar.length-1)){
   if(ar[i-1]==ar[i]){
-    count += 1;
-    i=i+2;
+    count += 1
   }
-  else
-    i=i+2;
-}    
-console.log(`count is ${count}`);
+  i=i+2
+}
+console.log(`total number of matching pairs of socks that Rohit can sell ${count}`);
